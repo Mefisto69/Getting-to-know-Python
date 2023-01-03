440dd5fcf0abbe65c35fedaa875b01862bd32bc8
Dmitriy <martin-hunt@list.ru>
2023-01-03 18:24:19 +0300

    Task_4_1-Fixed

--- Home_works/Home_work_4/HW_Task_4_1.py
+++ Home_works/Home_work_4/HW_Task_4_1.py
@@ -3,12 +3,15 @@
 # in  -> Enter a real number: 9
 #     -> Enter the required accuracy '0.0001': 0.000001
 # out -> 9.000000
+# in  -> Enter a real number: 8.98785
+#     -> Enter the required accuracy '0.0001': 0.001
+# out -> 8.988
 from decimal import *
 
 import os
 clear = lambda: os.system('cls')
 clear()
 
-user_number = Decimal(int(input('Enter a real number: ')))
+user_number = Decimal(input('Enter a real number: '))
 d_accuracy = Decimal(input('Enter the required accuracy "0.0001": '))
-print (user_number-d_accuracy+d_accuracy)
+print(user_number.quantize(d_accuracy))
