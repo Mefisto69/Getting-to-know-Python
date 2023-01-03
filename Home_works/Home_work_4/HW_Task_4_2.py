112dbe009341828c26a91f97a7666f8a098206e9
Dmitriy <martin-hunt@list.ru>
2023-01-03 18:59:07 +0300

    Task_4_2-Complete

--- Home_works/Home_work_4/HW_Task_4_2.py
+++ Home_works/Home_work_4/HW_Task_4_2.py
@@ -1,5 +1,21 @@
-
-
+# Задайте натуральное число N. 
+# Напишите программу, которая составит 
+# список простых множителей числа N.
+# in  -> 54
+# out -> [2, 3, 3, 3]
 import os
 clear = lambda: os.system('cls')
 clear()
+
+def get_mult_list(user_value:int):
+   i = 2
+   mult_list = []
+   while i * i <= user_value:
+       while user_value % i == 0:
+           mult_list.append(i)
+           user_value = user_value / i
+       i = i + 1
+   if user_value > 1:
+       mult_list.append(user_value)
+   return mult_list
+print(get_mult_list(int(input("Enter a value - "))))
